0f1943467700c33bcad58caa3de8f8810d5b54e6
Weldisson Araujo <weldisson.a.silva@accenture.com>
2024-07-15 10:03:53 -0300

    fix lint

--- src/application/ChatService.ts
+++ src/application/ChatService.ts
@@ -6,38 +6,38 @@ import type { FilesRepositoryDomain } from "../domain/FilesRepository";
 import logger from "../interfaces/helpers/Logger";
 
 export class ChatService {
-  constructor(private fileRepository: FilesRepositoryDomain) {}
+	constructor(private fileRepository: FilesRepositoryDomain) {}
 
-  async create(content: string): Promise<string> {
-    try {
-      logger.info("content is ", content);
-      await this.fileRepository.readFile();
+	async create(content: string): Promise<string> {
+		try {
+			logger.info("content is ", content);
+			await this.fileRepository.readFile();
 
-      const model = new BedrockChat({
-        model: "anthropic.claude-3-sonnet-20240229-v1:0",
-        region: process.env.BEDROCK_AWS_REGION as string,
-        credentials: {
-          accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID as string,
-          secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY as string,
-        },
-      });
+			const model = new BedrockChat({
+				model: "anthropic.claude-3-sonnet-20240229-v1:0",
+				region: process.env.BEDROCK_AWS_REGION as string,
+				credentials: {
+					accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID as string,
+					secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY as string,
+				},
+			});
 
-      const res = await model.invoke([
-        new HumanMessage({ content: "Tell me a joke" }),
-      ]);
-      logger.info(res);
+			const res = await model.invoke([
+				new HumanMessage({ content: "Tell me a joke" }),
+			]);
+			logger.info(res);
 
-      const stream = await model.stream([
-        new HumanMessage({ content: "Tell me a joke" }),
-      ]);
+			const stream = await model.stream([
+				new HumanMessage({ content: "Tell me a joke" }),
+			]);
 
-      for await (const chunk of stream) {
-        logger.info(chunk.content);
-      }
-      return "chat success";
-    } catch (error) {
-      logger.error(error);
-      return "chat error";
-    }
-  }
+			for await (const chunk of stream) {
+				logger.info(chunk.content);
+			}
+			return "chat success";
+		} catch (error) {
+			logger.error(error);
+			return "chat error";
+		}
+	}
 }
