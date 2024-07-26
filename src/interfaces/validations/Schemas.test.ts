18d5c93348fad8335d9859a8aeb4b49bc5821460
Weldisson Araujo <weldisson.a.silva@accenture.com>
2024-07-26 17:33:20 -0300

    add fileSchema test

--- src/interfaces/validations/Schemas.test.ts
+++ src/interfaces/validations/Schemas.test.ts
@@ -1,30 +1,40 @@
-import { chatSchema } from "./Schemas";
+import { chatSchema, fileSchema } from "./Schemas";
 
 describe("Schemas", () => {
-	describe("chatSchema", () => {
-		it("Should pass with correct data", () => {
-			const data = {
-				content: "Who invented the light bulb?",
-			};
+  describe("chatSchema", () => {
+    it("Should pass with correct data", () => {
+      const data = {
+        content: "Who invented the light bulb?",
+      };
 
-			expect(chatSchema.safeParse(data).success).toBe(true);
-		});
+      expect(chatSchema.safeParse(data).success).toBe(true);
+    });
 
-		it("'content' is invalid", () => {
-			const invalidData = {};
+    it("'content' is invalid", () => {
+      const invalidData = {};
 
-			const result = chatSchema.safeParse(invalidData);
-			expect(result.error?.errors[0].code).toBe("invalid_type");
-			expect(result.error?.errors[0].message).toBe("Required");
-			expect(result.error?.errors[0].path[0]).toBe("content");
-		});
-		it("'content' is not string", () => {
-			const invalidData = { content: 1 };
-			const result = chatSchema.safeParse(invalidData);
-			expect(result.error?.errors[0].message).toBe(
-				"Expected string, received number",
-			);
-			expect(result.error?.errors[0].path[0]).toBe("content");
-		});
-	});
+      const result = chatSchema.safeParse(invalidData);
+      expect(result.error?.errors[0].code).toBe("invalid_type");
+      expect(result.error?.errors[0].message).toBe("Required");
+      expect(result.error?.errors[0].path[0]).toBe("content");
+    });
+    it("'content' is not string", () => {
+      const invalidData = { content: 1 };
+      const result = chatSchema.safeParse(invalidData);
+      expect(result.error?.errors[0].message).toBe(
+        "Expected string, received number"
+      );
+      expect(result.error?.errors[0].path[0]).toBe("content");
+    });
+  });
+  describe("fileSchema", () => {
+    it("input is not instance of File", () => {
+      const invalidData = { file: 1 };
+      const result = fileSchema.safeParse(invalidData);
+      expect(result.error?.errors[0].message).toBe(
+        "Input not instance of File"
+      );
+      expect(result.error?.errors[0].path[0]).toBe("file");
+    });
+  });
 });
